7d0b471cfb93487803693f4d980ab208a3dc2428
Karlo Knezevic <knezevic.karlo1@gmail.com>
2016-12-19 12:15:50 +0100

    integration test

--- main.c
+++ main.c
@@ -13,16 +13,16 @@ int main(int argc, char *argv[]) {
 	struct parameters *params = NULL;
 	struct chromosome *chromo = NULL;
 
-//	int numInputs = 3;
-//	int numNodes = 5;
-//	int numOutputs = 2;
-//	int nodeArity = 2;
-//
-//	params = initialiseParameters(numInputs, numNodes, numOutputs, nodeArity);
-//
-//	addNodeFunction(params, "add,sub,mul");
-//
-//	chromo = initialiseChromosome(params);
+	int numInputs = 3;
+	int numNodes = 5;
+	int numOutputs = 2;
+	int nodeArity = 2;
+
+	params = initialiseParameters(numInputs, numNodes, numOutputs, nodeArity);
+
+	addNodeFunction(params, "add,sub,mul");
+
+	chromo = initialiseChromosome(params);
 
 	chromo = initialiseChromosomeFromFile("data/chromosome.txt");
 
