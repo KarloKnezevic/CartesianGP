8b02572ffe580c35ab96ee6e222f5a4d7c2dfcc1
Karlo Knezevic <knezevic.karlo1@gmail.com>
2017-01-20 16:50:33 +0100

    evaluator method implemented

--- src/machinelearning/evaluator.c
+++ src/machinelearning/evaluator.c
@@ -9,13 +9,31 @@
 #include "evaluator.h"
 
 #include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+#define _tp measures[EVAL_TP]
+#define _tn measures[EVAL_TN]
+#define _fp measures[EVAL_FP]
+#define _fn measures[EVAL_FN]
 
 //-----------------------------------------------------------------
 //                          CONSTRUCTOR
 //-----------------------------------------------------------------
 
 struct evaluator *_initialiseEvaluator(struct parameters *params) {
-	return NULL;
+	struct evaluator *eval = (struct evaluator *) malloc(
+			sizeof(struct evaluator));
+
+	eval->accuracy = 0.0;
+	eval->error = 0.0;
+	eval->sensitivity = 0.0;
+	eval->specificity = 0.0;
+	eval->precision = 0.0;
+	eval->DOR = 0.0;
+	eval->F1 = 0.0;
+
+	return eval;
 }
 
 //-----------------------------------------------------------------
@@ -40,7 +58,23 @@ void _validate(struct parameters *params, struct chromosome *chromo,
 //-----------------------------------------------------------------
 
 void _printEvaluator(struct evaluator *eval) {
+	if (eval == NULL) {
+		printf(
+				"Error: evaluator has not been initialised and cannot be printed.\n");
+		return;
+	}
 
+	printf("-----------------------------------------------------------\n");
+	printf("                     *Evaluator*                           \n");
+	printf("-----------------------------------------------------------\n");
+	printf("Accuracy:\t\t%f\n", eval->accuracy);
+	printf("Error:\t\t%f\n", eval->error);
+	printf("Sensitivity:\t\t%f\n", eval->sensitivity);
+	printf("Specificity:\t\t%f\n", eval->specificity);
+	printf("Precision:\t\t%f\n", eval->precision);
+	printf("DOR:\t\t%f\n", eval->DOR);
+	printf("F1:\t\t%f\n", eval->F1);
+	printf("-----------------------------------------------------------\n\n");
 }
 
 //-----------------------------------------------------------------
@@ -48,37 +82,134 @@ void _printEvaluator(struct evaluator *eval) {
 //-----------------------------------------------------------------
 
 void _freeEvaluator(struct evaluator *eval) {
-
+	free(eval);
 }
 
 //-----------------------------------------------------------------
 //                             UTILITY
 //-----------------------------------------------------------------
 
+int *_calculate_4_class(struct matrix *confusionMatrix, int class, int *res) {
+	if (NULL == res) {
+		res = (int *) calloc(4, sizeof(int));
+	}
+
+	for (int true = 0; true < confusionMatrix->rows; true++) {
+		for (int predicted = 0; predicted < confusionMatrix->cols;
+				predicted++) {
+
+			//TP
+			if (true == predicted) {
+				res[EVAL_TP] += confusionMatrix->data[true][predicted];
+			}
+
+			//TN
+			if (true != class && predicted != class) {
+				res[EVAL_TN] += confusionMatrix->data[true][predicted];
+			}
+
+			//FP
+			if (true != class && predicted == class) {
+				res[EVAL_FP] += confusionMatrix->data[true][predicted];
+			}
+
+			//FN
+			if (true == class && predicted != class) {
+				res[EVAL_FN] += confusionMatrix->data[true][predicted];
+			}
+		}
+	}
+
+	return res;
+}
+
 double _computeAccuracy(struct matrix *confusionMatrix) {
-	return 0.0;
+	if (NULL == confusionMatrix) {
+		return 0.0;
+	}
+
+	int *measures = NULL;
+	for (int class = 0; class < confusionMatrix->rows; class++) {
+		measures = _calculate_4_class(confusionMatrix, class, measures);
+	}
+
+	double acc = (_tp+ _tn) / (confusionMatrix->cols * (_tp + _tn + _fp + _fn));
+
+	free(measures);
+
+	return acc;
 }
 
 double _computeError(struct matrix *confusionMatrix) {
-	return 0.0;
+	return 1.0 - _computeAccuracy(confusionMatrix);
 }
 
 double _computeSensitivity(struct matrix *confusionMatrix) {
-	return 0.0;
+	if (NULL == confusionMatrix) {
+		return 0.0;
+	}
+
+	int *measures = NULL;
+	for (int class = 0; class < confusionMatrix->rows; class++) {
+		measures = _calculate_4_class(confusionMatrix, class, measures);
+	}
+
+	double sensitivity = (_tp) / (confusionMatrix->cols * (_tp + _fn));
+
+	free(measures);
+
+	return sensitivity;
 }
 
 double _computeSpecificity(struct matrix *confusionMatrix) {
-	return 0.0;
+	if (NULL == confusionMatrix) {
+		return 0.0;
+	}
+
+	int *measures = NULL;
+	for (int class = 0; class < confusionMatrix->rows; class++) {
+		measures = _calculate_4_class(confusionMatrix, class, measures);
+	}
+
+	double specificity = (_tn) / (confusionMatrix->cols * (_tn + _fp));
+
+	free(measures);
+
+	return specificity;
 }
 
 double _computePrecision(struct matrix *confusionMatrix) {
-	return 0.0;
+	if (NULL == confusionMatrix) {
+		return 0.0;
+	}
+
+	int *measures = NULL;
+	for (int class = 0; class < confusionMatrix->rows; class++) {
+		measures = _calculate_4_class(confusionMatrix, class, measures);
+	}
+
+	double precision = (_tp) / (confusionMatrix->cols * (_tp + _fp));
+
+	free(measures);
+
+	return precision;
 }
 
 double _computeDOR(struct matrix *confusionMatrix) {
-	return 0.0;
+	double sensitivity = _computeSensitivity(confusionMatrix);
+	double specificity = _computeSpecificity(confusionMatrix);
+
+	if (fabs(1 - sensitivity) < EPSILON || fabs(1 - specificity) < EPSILON) {
+		return -1;
+	}
+
+	return (sensitivity * specificity) / ((1 - sensitivity) * (1 - specificity));
 }
 
 double _computeF1(struct matrix *confusionMatrix) {
-	return 0.0;
+	double sensitivity = _computeSensitivity(confusionMatrix);
+	double specificity = _computeSpecificity(confusionMatrix);
+	double precision = _computePrecision(confusionMatrix);
+
+	return (2 * sensitivity * specificity) / (precision + sensitivity);
 }
--- src/machinelearning/evaluator.h
+++ src/machinelearning/evaluator.h
@@ -14,6 +14,11 @@
 #include "../chromosome.h"
 #include "../dataset.h"
 
+#define EVAL_TP 0
+#define EVAL_TN 1
+#define EVAL_FP 2
+#define EVAL_FN 3
+
 struct evaluator {
 	double accuracy;
 	double error;
@@ -61,16 +66,49 @@ void _freeEvaluator(struct evaluator *eval);
 //                             UTILITY
 //-----------------------------------------------------------------
 
+/**
+ * THE HIGHER, THE BETTER
+ */
 double _computeAccuracy(struct matrix *confusionMatrix);
 
+/**
+ * THE LOWER, THE BETTER
+ */
 double _computeError(struct matrix *confusionMatrix);
 
+/**
+ * THE HIGHER, THE BETTER
+ *
+ * This measure is appropriate if it's important to predict TRUE values.
+ */
 double _computeSensitivity(struct matrix *confusionMatrix);
 
+/**
+ * THE HIGHER, THE BETTER
+ *
+ * This measure is appropriate if it's important to predict NEGATIVE values.
+ */
 double _computeSpecificity(struct matrix *confusionMatrix);
 
+/**
+ * THE HIGHER, THE BETTER
+ *
+ * This measure calculates the probability that predicted value is true.
+ */
 double _computePrecision(struct matrix *confusionMatrix);
 
+/**
+ * THE HIGHER, THE BETTER
+ *
+ * The diagnostic odds ratio ranges from zero to infinity,
+ * although for useful tests it is greater than one, and higher
+ * diagnostic odds ratios are indicative of better test performance.
+ * Diagnostic odds ratios less than one indicate that the test can be
+ * improved by simply inverting the outcome of the test – the test is in
+ * the wrong direction, while a diagnostic odds ratio of exactly one
+ * means that the test is equally likely to predict a positive outcome
+ * whatever the true condition – the test gives no information.
+ */
 double _computeDOR(struct matrix *confusionMatrix);
 
 double _computeF1(struct matrix *confusionMatrix);
